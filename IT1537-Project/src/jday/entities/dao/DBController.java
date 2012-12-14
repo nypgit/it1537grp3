41747295fe3be484b65f049192891dda5d29d0fd
Deane <Deane@Deane-HP>
2012-12-14 20:25:47 +0800

    database is working now

--- IT1537-Project/src/jday/entities/dao/DBController.java
+++ IT1537-Project/src/jday/entities/dao/DBController.java
@@ -9,7 +9,7 @@ import java.sql.DriverManager;
 
 public class DBController {
 	private Connection con;
-	private String dbSource="//172.27.176.139:3306/jdaydatabase";
+	private String dbSource="//192.168.0.106.60:3306/jdaydatabase";
 	private String user="myuser";	  // the username of the sql user
 	private String password="jday";  // your password of the sql user
 	
@@ -90,7 +90,7 @@ public class DBController {
 		try {
 			// step 1
 			con=DriverManager.getConnection(
-					"jdbc:mysql://172.27.176.139:3306/jdaydatabase","myuser","jday");
+					"jdbc:mysql://192.168.0.106:3306/jdaydatabase","myuser","jday");
 					
 			// step 2
 			stmt=con.createStatement();
